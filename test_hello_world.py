37324c73e5c8c07d44a5f72444536c93910db62d
Angel Leonardo Bianco <angel.leonardo.bianco@gmail.com>
2023-06-30 13:52:47 -0300

    Update test_hello_world.py

--- test_hello_world.py
+++ test_hello_world.py
@@ -2,5 +2,5 @@ import unittest
 
 class TestHelloWorld(unittest.TestCase):
   def test_hello_world(self):
-    from hello_world import hello_world
-    self.assertEqual(hello_world(), "Hello, World!")
+    from helloWorld import helloWorld
+    self.assertEqual(helloWorld(), "Hello, World!")
